27e5a7fa40b99123d76ca89cdf6319089b9e4d73
Sam Richard <sam@snug.ug>
2017-09-18 10:58:16 -0500

    :tada: Wrap Normalize with Eyeglass module

---
+++ README.md
@@ -0,0 +1,17 @@
+# Normalize Eyeglass
+
+A simple [Eyeglass](https://github.com/sass-eyeglass/eyeglass) wrapper for [Normalize.css](https://github.com/necolas/normalize.css)
+
+## Usage
+
+Presuming Eyeglass is already installed and configured, install this module as follows:
+
+```bash
+npm install normalize-eyeglass --save-dev
+```
+
+Then, in a Sass file, import normalize
+
+```scss
+@import 'normalize';
+```
---
+++ to-sass.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var getInstalledPath = require('get-installed-path');
+
+getInstalledPath('normalize.css', {
+  local: true,
+}).then(locale => {
+  // Output file paths
+  var input = path.join(locale, 'normalize.css');
+  var output = path.join(__dirname, '_normalize.scss');
+
+  // Clean up Sass file if it's already there
+  try {
+    fs.statSync(output);
+    fs.unlinkSync(output);
+    console.log('Removed old version of _normalize.scss');
+  } catch(e) {
+    // It's OK if this errors, it means the file doesn't exist
+  }
+
+  // Cpoy input file to output file
+  fs.writeFileSync(output, fs.readFileSync(input));
+  console.log('Copied normalize.css to _normalize.scss');
+}).catch(e => {
+  console.error(e);
+});
